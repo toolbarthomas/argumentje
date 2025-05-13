7ecb73ebf736c2b81daa00bc663370f57793bae5
Thomas van der Velde <contact@toolbarthomas.net>
2025-05-13 19:04:53 +0200

    chore(node): Maintenance

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
-export type V = boolean | number | string | undefined | null;
-export type Response = { [key: string]: V };
-export type Parser = (value?: string[]) => Response;
+export type V = boolean | number | string | undefined | null
+export type Response = Record<string, V>
+export type Parser = (value?: string[]) => Response
 
 /**
  * Simple Command Line Interface parser for Node.js
@@ -11,59 +11,59 @@ export type Parser = (value?: string[]) => Response;
  * - Flag
  * - String
  */
-export const parse: Parser = (value) => {
+export const parse = <T = Response>(value?: string[]) => {
   //@ts-ignore
-  const args = value || process.argv.slice(2).reverse();
-  const response: Response = {};
+  const args = value || process.argv.slice(2).reverse()
+  const response = {} as any
 
   try {
     for (let i = args.length; i--; ) {
-      let [current] = args[i].split("-").filter((e) => e);
+      let [current] = args[i].split('-').filter((e) => e)
 
-      const arg = current && current.split("=");
+      const arg = current && current.split('=')
 
       if (arg === undefined) {
-        continue;
+        continue
       }
 
-      const [name, ...rest] = arg;
+      const [name, ...rest] = arg
 
       if (name === undefined) {
-        continue;
+        continue
       }
 
-      const [initial] = rest || [];
-      let v: any = initial;
+      const [initial] = rest || []
+      let v: any = initial
 
-      if (!args[i].startsWith("-") && typeof current === "string") {
-        v = current;
+      if (!args[i].startsWith('-') && typeof current === 'string') {
+        v = current
       } else if (v !== undefined) {
-        if (v === "null") {
-          v = null;
-        } else if (String(v).toUpperCase() === "TRUE") {
-          v = true;
-        } else if (String(v).toUpperCase() === "FALSE") {
-          v = false;
+        if (v === 'null') {
+          v = null
+        } else if (String(v).toUpperCase() === 'TRUE') {
+          v = true
+        } else if (String(v).toUpperCase() === 'FALSE') {
+          v = false
         } else if (parseInt(v)) {
-          v = parseInt(v);
+          v = parseInt(v)
         }
       }
 
-      while (current.startsWith("-")) {
-        current = current.slice(1);
+      while (current.startsWith('-')) {
+        current = current.slice(1)
       }
 
-      response[name] = v === undefined ? true : v;
+      response[name as any] = v === undefined ? true : v
     }
   } catch (exception) {
     if (exception) {
-      return {};
+      return {} as T
     }
   }
 
-  return response;
-};
+  return response
+}
 
 export default {
-  parse,
-};
+  parse
+}
